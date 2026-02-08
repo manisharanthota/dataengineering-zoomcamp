8c37bd4468e742f2cbb808aa0e9f0394039d3eba
Manisharan Thota <116102968+manisharanthota@users.noreply.github.com>
2026-02-08 04:11:02 +0000

    Updated

--- pipeline/ingest_data.py
+++ pipeline/ingest_data.py
@@ -1,39 +1,10 @@
 #!/usr/bin/env python
 # coding: utf-8
-
-# In[1]:
-
-
+import click
 import pandas as pd
+from sqlalchemy import create_engine
+from tqdm.auto import tqdm
 
-
-# In[2]:
-
-
-prefix = 'https://github.com/DataTalksClub/nyc-tlc-data/releases/download/yellow/'
-url = f'{prefix}/yellow_tripdata_2021-01.csv.gz'
-url
-
-
-# In[3]:
-
-
-df = pd.read_csv(url)
-
-
-# In[4]:
-
-
-df.dtypes
-
-
-# In[5]:
-
-
-df.shape
-
-
-# In[6]:
 
 
 dtype = {
@@ -60,116 +31,73 @@ parse_dates = [
     "tpep_dropoff_datetime"
 ]
 
-df = pd.read_csv(
-    url,
-    dtype=dtype,
-    parse_dates=parse_dates
-)
 
+def ingest_data(
+        url: str,
+        engine,
+        target_table: str,
+        chunksize: int = 100000,
+) -> pd.DataFrame:
+    df_iter = pd.read_csv(
+        url,
+        dtype=dtype,
+        parse_dates=parse_dates,
+        iterator=True,
+        chunksize=chunksize
+    )
 
-# In[ ]:
+    first_chunk = next(df_iter)
 
+    first_chunk.head(0).to_sql(
+        name=target_table,
+        con=engine,
+        if_exists="replace"
+    )
 
+    print(f"Table {target_table} created")
 
-
-
-# In[7]:
-
-
-df['tpep_pickup_datetime']
-
-
-# In[8]:
-
-
-get_ipython().system('uv add sqlalchemy psycopg2-binary')
-
-
-# In[9]:
-
-
-from sqlalchemy import create_engine
-engine = create_engine('postgresql://root:root@localhost:5432/ny_taxi')
-
-
-# In[10]:
-
-
-df.head(n=0).to_sql(name='yellow_taxi_data', con=engine, if_exists='replace')
-
-
-# In[18]:
-
-
-df_iter  = pd.read_csv(
-    url,
-    dtype=dtype,
-    parse_dates=parse_dates,
-    iterator = True,
-    chunksize = 100000
-)
-
-
-# In[12]:
-
-
-for df_chunk in df_iter:
-    print(len(df_chunk))
-
-
-# In[13]:
-
-
-get_ipython().system('uv add tqdm')
-
-
-# In[19]:
-
-
-first = True
-
-for df_chunk in df_iter:
-
-    if first:
-        # Create table schema (no data)
-        df_chunk.head(0).to_sql(
-            name="yellow_taxi_data",
-            con=engine,
-            if_exists="replace"
-        )
-        first = False
-        print("Table created")
-
-    # Insert chunk
-    df_chunk.to_sql(
-        name="yellow_taxi_data",
+    first_chunk.to_sql(
+        name=target_table,
         con=engine,
         if_exists="append"
     )
 
-    print("Inserted:", len(df_chunk))
+    print(f"Inserted first chunk: {len(first_chunk)}")
 
+    for df_chunk in tqdm(df_iter):
+        df_chunk.to_sql(
+            name=target_table,
+            con=engine,
+            if_exists="append"
+        )
+        print(f"Inserted chunk: {len(df_chunk)}")
 
-# In[ ]:
+    print(f'done ingesting to {target_table}')
 
+@click.command()
+@click.option('--pg-user', default='root', help='PostgreSQL username')
+@click.option('--pg-pass', default='root', help='PostgreSQL password')
+@click.option('--pg-host', default='localhost', help='PostgreSQL host')
+@click.option('--pg-port', default='5432', help='PostgreSQL port')
+@click.option('--pg-db', default='ny_taxi', help='PostgreSQL database name')
+@click.option('--year', default=2021, type=int, help='Year of the data')
+@click.option('--month', default=1, type=int, help='Month of the data')
+@click.option('--chunksize', default=100000, type=int, help='Chunk size for ingestion')
+@click.option('--target-table', default='yellow_taxi_data', help='Target table name')
+def main(pg_user, pg_pass, pg_host, pg_port, pg_db, year, month, chunksize, target_table):
+    
 
+    engine = create_engine(f'postgresql://{pg_user}:{pg_pass}@{pg_host}:{pg_port}/{pg_db}')
+    url_prefix = 'https://github.com/DataTalksClub/nyc-tlc-data/releases/download/yellow'
 
+    url = f'{url_prefix}/yellow_tripdata_{year:04d}-{month:02d}.csv.gz'
 
+    ingest_data(
+        url=url,
+        engine=engine,
+        target_table=target_table,
+        chunksize=chunksize
+    )
 
-# In[ ]:
-
-
-
-
-
-# In[ ]:
-
-
-
-
-
-# In[ ]:
-
-
-
-
+if __name__ == '__main__':
+    main()
